1041f35de44268b27ebdc0767b57bb5d75235b72
Faydee <faydee1220@gmail.com>
2018-03-01 09:51:44 +0800

    新增 SharedPreferences, menu
    關閉後再打開（或轉向）時，仍保有原先輸入資料；可清除全部已輸入資料

--- app/src/main/java/com/rq/golfscorecard/MainActivity.java
+++ app/src/main/java/com/rq/golfscorecard/MainActivity.java
@@ -1,9 +1,13 @@
 package com.rq.golfscorecard;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.rq.golfscorecard.adapters.ListAdapter;
 import com.rq.golfscorecard.model.Hole;
@@ -13,17 +17,25 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Hole[] holes = new Hole[18]; // 高爾夫球的 18 洞
+    private static final String PREFS_FILE = "com.rq.golfscorecard.preferences";
+    private static final String PREF_HOLE = "PREF_HOLE";
     @BindView(R.id.recyclerView) RecyclerView recyclerView;
+
+    public Hole[] holes = new Hole[18]; // 高爾夫球的 18 洞
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        sharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
 
         int strokes = 0;
         for (int i = 0; i < holes.length; i += 1) {
+            strokes = sharedPreferences.getInt(PREF_HOLE + i + 1, 0);
             holes[i] = new Hole("Hole " + (i + 1) + " :", strokes);
         }
 
@@ -33,7 +45,38 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-//        recyclerView.setHasFixedSize(true);
+        recyclerView.setHasFixedSize(true);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        editor = sharedPreferences.edit();
+        for (int i = 0; i < holes.length; i += 1) {
+            editor.putInt(PREF_HOLE + i + 1, holes[i].getStrokeCount());
+        }
+        editor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.clearStorkes) {
+            for (Hole hole : holes) {
+                hole.setStrokeCount(0);
+            }
+            editor = sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
+            recyclerView.getAdapter().notifyDataSetChanged();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
